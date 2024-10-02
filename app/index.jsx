fa61b5087d52095c63bad4236bcf9757b953e31d
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-02 10:28:24 +0530

    app commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Welcome to BayMax</Text>
+    </View>
+  );
+};
+
+export default index;
