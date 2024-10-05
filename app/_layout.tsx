0a578a1b9947f0746c52bfb0de202abacba45636
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-05 16:17:02 +0530

    app commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import "react-native-reanimated";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="app" options={{ headerShown: false }} />
     </Stack>
   );
 }
