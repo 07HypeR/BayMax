4e28a31c8c8e125ee2aab1872234816a75cd4617
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-05 15:47:20 +0530

    app commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,8 @@
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+
 import "react-native-reanimated";
 
-SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/Bangers-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
