6e028d804c8b022fe9aef553931e26ba8941e535
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-07 21:22:10 +0530

    app commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,14 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import { router } from "expo-router";
+import Animated from "react-native-reanimated";
 
 const SplashScreen = () => {
   return (
     <View className="flex-1 items-center justify-center  bg-primary">
-      <TouchableOpacity onPress={() => router.replace("/BaymaxScreen")}>
-        <Text className="text-2xl font-Theme">SplashScreen</Text>
-      </TouchableOpacity>
+      <Animated.View>
+        <Image source={require("../assets/images/launch.png")} />
+      </Animated.View>
     </View>
   );
 };
