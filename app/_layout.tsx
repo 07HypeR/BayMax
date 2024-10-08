628437c882a66481958a88543a4ed4c5f66f0336
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-08 18:56:10 +0530

    app commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,34 @@
-import { Stack } from "expo-router";
+import { Stack, SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import React, { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Bangers-Regular": require("../assets/fonts/Bangers-Regular.ttf"),
+    "Okra-Bold": require("../assets/fonts/Okra-Bold.ttf"),
+    "Okra-Medium": require("../assets/fonts/Okra-Medium.ttf"),
+    "Okra-MediumLight": require("../assets/fonts/Okra-MediumLight.ttf"),
+    "Okra-Regular": require("../assets/fonts/Okra-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- app/index.tsx
+++ app/index.tsx
@@ -2,9 +2,13 @@ import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import React from "react";
 import { router } from "expo-router";
 import Animated from "react-native-reanimated";
-import { Colors } from "@/utils/Constants";
+import { Colors, Fonts, lightColors } from "@/utils/Constants";
 import { screenHeight, screenWidth } from "@/utils/Scaling";
+import { LinearGradient } from "expo-linear-gradient";
+import CustomText from "@/components/global/CustomText";
+import LottieView from "lottie-react-native";
 
+const bottomColors = [...lightColors].reverse();
 const SplashScreen = () => {
   return (
     <View style={styles.container}>
@@ -14,11 +18,38 @@ const SplashScreen = () => {
           style={styles.img}
         />
       </Animated.View>
+
+      <Animated.View style={styles.gradientContainer}>
+        <LinearGradient colors={bottomColors} style={styles.gradient}>
+          <View>
+            <CustomText fontSize={34} fontFamily={Fonts.Theme}>
+              BAYMAX!
+            </CustomText>
+            <LottieView
+              source={require("../assets/animations/sync.json")}
+              style={{ width: 280, height: 100 }}
+              autoPlay={true}
+              loop
+            />
+          </View>
+        </LinearGradient>
+      </Animated.View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  gradientContainer: {
+    position: "absolute",
+    height: "35%",
+    bottom: 0,
+    width: "100%",
+  },
+  gradient: {
+    paddingTop: 30,
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
     backgroundColor: Colors.primary,
