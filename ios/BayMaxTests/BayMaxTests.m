29b262d1b2062d3ea574c617144edaeed91a33e5
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-11 00:49:30 +0530

    app commit

--- ios/BayMaxTests/BayMaxTests.m
+++ ios/BayMaxTests/BayMaxTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BayMaxTests : XCTestCase
+@interface baymaxTests : XCTestCase
 
 @end
 
-@implementation BayMaxTests
+@implementation baymaxTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
