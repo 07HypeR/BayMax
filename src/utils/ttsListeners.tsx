cdec4e553f6bb3704800ad16b45c88d2ff5bea25
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-11 13:17:02 +0530

    speaking model added

--- src/utils/ttsListeners.tsx
+++ src/utils/ttsListeners.tsx
@@ -1,18 +1,38 @@
-import Tts from "react-native-tts";
+import Tts from 'react-native-tts';
 
 export const initializeTtsListeners = async () => {
   Tts.getInitStatus().then(
     () => {
-      console.log("ALL OK TTS ✅");
+      console.log('ALL OK TTS ✅');
     },
-    (err) => {
-      if (err.code === "no_engine") {
-        console.log("NO TTS ENGINE ❌ ");
+    err => {
+      if (err.code === 'no_engine') {
+        console.log('NO TTS ENGINE ❌ ');
         Tts.requestInstallEngine();
       }
-    }
+    },
   );
 
-  const voices = await Tts.voices();
-  console.log(voices);
+  // const voices = await Tts.voices();
+  // console.log(voices);
+
+  Tts.setDefaultRate(0.7, true);
+  Tts.setIgnoreSilentSwitch('ignore');
+
+  Tts.setDefaultPitch(0.7);
+  Tts.addEventListener('tts-start', event => {
+    console.log('TTS Started: ', event);
+  });
+  Tts.setDefaultPitch(0.7);
+  Tts.addEventListener('tts-progress', event => {
+    // console.log('TTS Progress: ', event);
+  });
+  Tts.setDefaultPitch(0.7);
+  Tts.addEventListener('tts-finish', event => {
+    console.log('TTS Finished: ', event);
+  });
+  Tts.setDefaultPitch(0.7);
+  Tts.addEventListener('tts-cancel', event => {
+    console.log('TTS Cancel: ', event);
+  });
 };
