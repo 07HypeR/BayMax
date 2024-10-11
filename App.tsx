db924ab4472284deddf66a14243b4596a5196a31
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-11 12:03:31 +0530

    app commit

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {RFValue} from 'react-native-responsive-fontsize';
+import Navigation from './src/navigation/Navigation';
 
 const App = () => {
-  return (
-    <View
-      style={{
-        justifyContent: 'center',
-        alignItems: 'center',
-        flex: 1,
-      }}>
-      <Icon name="arrow-down" size={RFValue(20)} color="white" />
-    </View>
-  );
+  return <Navigation />;
 };
 
 export default App;
