d6d2bed3a0605f211e26cc5091d969f60c960b8e
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-18 20:31:01 +0530

    Gemini added

--- src/components/pedometer/Pedometer.tsx
+++ src/components/pedometer/Pedometer.tsx
@@ -1,12 +1,50 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import React, {FC} from 'react';
+import {RFValue} from 'react-native-responsive-fontsize';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Pedometer = () => {
+const Pedometer: FC<{
+  message: string;
+  onCross: () => void;
+}> = ({message, onCross}) => {
   return (
-    <View>
-      <Text>Pedometer</Text>
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.cross} onPress={onCross}>
+        <Icon name="close-circle" color="red" size={RFValue(20)} />
+      </TouchableOpacity>
+      <Image
+        source={require('../../assets/images/logo_short.png')}
+        style={styles.logo}
+      />
+      <View></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 10,
+    width: '90%',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.08,
+    shadowRadius: 16,
+    elevation: 10,
+    shadowColor: '#000',
+    borderRadius: 10,
+  },
+  logo: {
+    width: 50,
+    height: 40,
+    alignSelf: 'center',
+    marginVertical: 10,
+  },
+  cross: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+  },
+});
 
 export default Pedometer;
