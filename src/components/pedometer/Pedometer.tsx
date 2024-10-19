c9774cbcfa477fcda99a09f95d368cd2c05cc8bb
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-19 21:37:42 +0530

    pedometer added

--- src/components/pedometer/Pedometer.tsx
+++ src/components/pedometer/Pedometer.tsx
@@ -1,22 +1,96 @@
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
-import React, {FC} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import React, {FC, useEffect} from 'react';
 import {RFValue} from 'react-native-responsive-fontsize';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {usePedometerStore} from '../../state/pedometerStore';
+import StepCounter, {
+  parseStepData,
+  startStepCounterUpdate,
+  stopStepCounterUpdate,
+} from '@dongminyu/react-native-step-counter';
+import {playTTS} from '../../utils/ttsListeners';
+import CircularProgress from 'react-native-circular-progress-indicator';
+import {Fonts} from '../../utils/Constants';
+import CustomText from '../global/CustomText';
 
 const Pedometer: FC<{
   message: string;
   onCross: () => void;
 }> = ({message, onCross}) => {
+  const {stepCount, dailyGoal, addSteps} = usePedometerStore();
+
+  StepCounter.addListener('StepCounter.stepsSensorInfo');
+  const startStepCounter = () => {
+    startStepCounterUpdate(new Date(), data => {
+      const parsedData = parseStepData(data);
+      addSteps(parsedData.steps, parsedData.distance);
+    });
+  };
+
+  const stopStepCounter = () => {
+    stopStepCounterUpdate();
+  };
+
+  useEffect(() => {
+    if (stepCount >= dailyGoal) {
+      playTTS(
+        "You've met your daily goal. No need to start the counter again today",
+      );
+    } else {
+      startStepCounter();
+    }
+
+    return () => {
+      stopStepCounter();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.cross} onPress={onCross}>
+      <TouchableOpacity
+        style={styles.cross}
+        onPress={() => {
+          Alert.alert('Your step counter stopped!!');
+          stopStepCounter();
+          onCross();
+        }}>
         <Icon name="close-circle" color="red" size={RFValue(20)} />
       </TouchableOpacity>
       <Image
         source={require('../../assets/images/logo_short.png')}
         style={styles.logo}
       />
-      <View></View>
+      <View style={styles.indicator}>
+        <CircularProgress
+          value={stepCount}
+          maxValue={dailyGoal}
+          valueSuffix="/2000"
+          progressValueFontSize={22}
+          radius={120}
+          activeStrokeColor="#cdd27e"
+          inActiveStrokeColor="#4c6394"
+          inActiveStrokeOpacity={0.5}
+          inActiveStrokeWidth={20}
+          activeStrokeWidth={20}
+          title="Steps"
+          titleColor="#555"
+          titleFontSize={22}
+          titleStyle={{fontFamily: Fonts.SemiBold}}
+        />
+        <CustomText
+          fontSize={RFValue(8)}
+          fontFamily={Fonts.SemiBold}
+          style={styles.text}>
+          Start Walking, counter will update automatically.
+        </CustomText>
+      </View>
     </View>
   );
 };
@@ -33,6 +107,17 @@ const styles = StyleSheet.create({
     elevation: 10,
     shadowColor: '#000',
     borderRadius: 10,
+  },
+  indicator: {
+    marginTop: 10,
+    marginBottom: 20,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    marginTop: 20,
+    textAlign: 'center',
   },
   logo: {
     width: 50,
