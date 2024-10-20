7a6b3df9597652fb63e1597aa65c861804bae304
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-20 23:36:33 +0530

    notification sound added

--- src/notification/notificationInnitial.tsx
+++ src/notification/notificationInnitial.tsx
@@ -3,3 +3,26 @@ import notifee from '@notifee/react-native';
 export const addBadgeCount = async () => {
   notifee.setBadgeCount(1).then(() => console.log('Badge Count'));
 };
+
+export const displayNotification = async (
+  title: string,
+  message: string,
+  image: string,
+  categoryId: string,
+) => {
+  const channelId = await notifee.createChannel({
+    id: 'default',
+    name: 'Defaul Channel',
+    sound: 'notification',
+  });
+
+  await notifee.displayNotification({
+    title: title,
+    body: message,
+    android: {
+      channelId: categoryId,
+      sound: 'notification',
+      onlyAlertOnce: true,
+    },
+  });
+};
