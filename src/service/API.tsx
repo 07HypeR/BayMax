e51fcbd8fc3f1d5ce3229cdc767fe023f32ef3ce
Abhik Baidya <abhikbaidya7@gmail.com>
2024-10-22 20:05:08 +0530

    Broadcast notification added

--- src/service/API.tsx
+++ src/service/API.tsx
@@ -1,1 +1,8 @@
-export const GEMINI_API_KEY = '';
+import {Platform} from 'react-native';
+
+export const GEMINI_API_KEY = 'AIzaSyD-C4LaCz2mMazc0m3pjA5dnyJdWXX5F88';
+// export const BASE_URL =
+//   Platform.OS == 'ios' ? 'http://localhost:3000' : 'http://10.0.2.2:3000';
+
+// FOR HOSTED
+export const BASE_URL = 'https://fcm-server-live-tjrb.onrender.com';
